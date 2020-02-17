d6264c5ad97a336f3100de8cb43bb708b6014a9d
Sergio <spablosayuso@infomm.local>
2020-02-17 20:07:11 +0100

    versión final de la build de producción

---
+++ precache-manifest.b80a7a85f91160b78297d92719c2a95b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46c0369a5d503f365103e636fb28862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4f78090590fb4c8e54c",
+    "url": "/static/css/main.9ac8762c.chunk.css"
+  },
+  {
+    "revision": "1bcf4ae4dd0153df06c2",
+    "url": "/static/js/2.6cd07ff3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6cd07ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f78090590fb4c8e54c",
+    "url": "/static/js/main.ccec8f08.chunk.js"
+  },
+  {
+    "revision": "0bc82b6b4d091f3848cd",
+    "url": "/static/js/runtime-main.1d050d9e.js"
+  },
+  {
+    "revision": "7e3ac7fa65a1c43aaf88f5b485ff2cd4",
+    "url": "/static/media/Paisaje1.7e3ac7fa.jpg"
+  },
+  {
+    "revision": "e9ef8e4260a780acb36c7b025ab12df6",
+    "url": "/static/media/Paisaje2.e9ef8e42.jpg"
+  },
+  {
+    "revision": "620d5e0e9adcc95e415f650eaf951e0c",
+    "url": "/static/media/Paisaje3.620d5e0e.jpg"
+  },
+  {
+    "revision": "b02dd1b692b66a8a07151390ce1d1c19",
+    "url": "/static/media/Paisaje4.b02dd1b6.jpg"
+  },
+  {
+    "revision": "12287c7f32eac56c2ddb199bb78b2336",
+    "url": "/static/media/Paisaje5.12287c7f.jpg"
+  },
+  {
+    "revision": "1b7a1f301f8bb6ce712e23901a16bbc9",
+    "url": "/static/media/Paisaje6.1b7a1f30.jpg"
+  },
+  {
+    "revision": "e8755a168efd26d31751ef573976af2d",
+    "url": "/static/media/Paisaje7.e8755a16.jpg"
+  },
+  {
+    "revision": "184df79ee30f62531cae563684f3eca7",
+    "url": "/static/media/Paisaje8.184df79e.jpg"
+  },
+  {
+    "revision": "893a1e4fcf10f59bfa6c8a5da217e50a",
+    "url": "/static/media/imagen1.893a1e4f.jpg"
+  },
+  {
+    "revision": "971eede1499c0a3de7fae66d81e1d4d6",
+    "url": "/static/media/imagen2.971eede1.PNG"
+  }
+]);
